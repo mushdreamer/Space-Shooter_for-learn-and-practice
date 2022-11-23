459b358731f53f7b44a92e44df12ca958914e2e9
Git Try <xcy010501@gmail.com>
2022-11-22 19:21:12 -0800

    Input

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    float speed = 3.5f;
+    float getHorizontal;
+    float getVertical;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        getHorizontal = Input.GetAxis("Horizontal");
+        getVertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * getHorizontal);
+        transform.Translate(Vector3.up * Time.deltaTime * speed * getVertical);
     }
 }
