47c8c79e3fc81234cd1499789339bd559756409e
Git <screamingbone.studio@gmail.com>
2023-03-13 16:02:38 -0700

    Enemy Shoot

--- Assets/Scripts/Bullets.cs
+++ Assets/Scripts/Bullets.cs
@@ -6,8 +6,21 @@ public class Bullets : MonoBehaviour
 {
     private float Speed = 7.0f;
 
+    private bool isEnemyBullets = false;
+
     // Update is called once per frame
     void Update()
+    {
+        if (isEnemyBullets == false)
+        {
+            MoveUp();
+        }
+        else
+        {
+            MoveDown();
+        }
+    }
+    private void MoveUp()
     {
         transform.Translate(Vector3.up * Speed * Time.deltaTime);
         /*概念
@@ -30,9 +43,31 @@ public class Bullets : MonoBehaviour
             }
             Destroy(this.gameObject);
         }
-
     }
+    private void MoveDown()
+    {
+        transform.Translate(Vector3.down * Speed * Time.deltaTime);
+        /*概念
+          Destroy
+          Removes a GameObject, component or asset.使一个GameObject在unity中消失
+          this.gameObject
+          this特指这个script中的game object也就是bullets*/
+        if (transform.position.y > 7.7f)
+        {
 
+            /*概念
+              Parent
+              Parent是一个Gameobject的概念，任何Gameobject都可以成为其它Gameobject的Parent或Child，他们之间的关系就像身体，胳膊，以及手一样，身体就像是胳膊的
+              Parent，而胳膊是身体的Child同时也是手的Parent，而手是胳膊的Child。具有这种关系的Gameobject互相继承特性，想要查询Gameobject的relationship只需要
+              在unity中的prefab中点击一个Gameobject然后open即可看到Hierarchy，Child会被Parent折叠起来
+              当TripleShot移动的时候，根据定义它的Child Bullet也会移动，因此我们在Bullet超过7.7f时检测这个Bullet是否有Parent，如果有的话直接destroy*/
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(this.gameObject);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Enemy")
@@ -40,5 +75,9 @@ public class Bullets : MonoBehaviour
             Destroy(this.gameObject);
             /*Destroy(other.gameObject);*/
         }
+    }
+    public void PlayerBullets()
+    {
+        isEnemyBullets = true;
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,10 +12,16 @@ public class Enemy : MonoBehaviour
 
     private Player player;
 
+    private float canFire = -1;
+
+    private float fireRate;
+
     [SerializeField]
     private AudioClip Enemy_Destroy;
     [SerializeField]
     private AudioSource Enemy_Destroy_Source;
+    [SerializeField]
+    private GameObject Enemy_Bullets;
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
@@ -28,13 +34,50 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        calculateMovemnt();
+        calculateBullets();
+
+    }
+    public void calculateMovemnt()
+    {
         transform.Translate(Vector3.down * speed * Time.deltaTime);
 
         if (transform.position.y < -4.296614f)
         {
             Destroy(this.gameObject);
         }
+    }
+    //让开枪状态为-1，而游戏开始时时长为0，根据if条件如果游戏时长大于开枪状态那么就可以发射一颗子弹，一开始0>1因此可以开枪，之后开枪间隔为3和7之间的任意数，我们
+    //让开枪状态等于当前的游戏时长加上开枪间隔，假设此时游戏市场为20秒，开枪时长为4秒，那么开枪状态就等于24秒，也就是说游戏时长20秒小于开枪状态24秒，那么要等24
+    //秒时才可以开枪，从而实现了4秒的开枪冷却
+    public void calculateBullets()
+    {
+        if(Time.time >= canFire)
+        {
+            fireRate = UnityEngine.Random.Range(3f, 7f);
+            canFire = Time.time + fireRate;
+            /*let's say you are creating a game where the player has to shoot down enemy spaceships. When an enemy spaceship is destroyed,
+            it needs to release bullets that will fly in different directions. In this case, you would have a prefab for the enemy spaceship
+            and a separate prefab for its bullets.
 
+            To create the enemy bullets, you would use the Instantiate() function to create a new instance of the enemy bullet prefab at the
+            current position of the enemy spaceship. The line of code GameObject EnemyBullets = Instantiate(Enemy_Bullets,transform.position,
+            Quaternion.identity); does exactly this.
+
+            After creating the enemy bullet object, you would then use the GetComponentsInChildren<Bullets>() function to get an array of all
+            the child objects that have the "Bullets" component attached. This is useful because the enemy bullet object may have multiple
+            child objects, each of which will need to perform some action.
+
+            Finally, the for loop is used to loop through each child object that has the "Bullets" component and call the PlayerBullets()
+            function on each of them. This function could be used to set the initial velocity of the bullets, give them a random direction
+            to fly in, or any other behavior that you want the bullets to have.*/
+            GameObject EnemyBullets = Instantiate(Enemy_Bullets, transform.position, Quaternion.identity);/*我们在原有的子弹基础上，又创建了一个新的具有子弹性质的敌人子弹*/
+            Bullets[] bullets = EnemyBullets.GetComponentsInChildren<Bullets>();/*然后将每一个生成的敌人子弹都存储在一个Array里*/
+            for (int i = 0; i < bullets.Length; i++)
+            {
+                bullets[i].PlayerBullets();
+            }/*遍历这个Array一一赋予他们性质,这样我们就可以在不影响其他子弹的前提下，控制敌人子弹的行为和属性。*/
+        }
     }
 
     /*概念
